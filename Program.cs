bf71319482339a8599b40d0671e1e6856fdf4ea5
Asus <Asus@DESKTOP-2RHI8S2>
2026-03-20 17:17:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StoreSalesSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
