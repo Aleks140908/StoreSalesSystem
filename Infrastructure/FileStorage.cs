ef25589beb064996cc88dffe4e0a354be77eecf2
Asus <Asus@DESKTOP-2RHI8S2>
2026-05-03 22:19:48 +0300

    .

---
+++ Infrastructure/FileStorage.cs
@@ -0,0 +1,50 @@
+﻿using StoreSalesSystem.Application;
+using StoreSalesSystem.Domain.Entities;
+using System.Text.Json;
+
+
+namespace StoreSalesSystem.Infrastructure
+{
+    public class FileStorage
+    {
+        public List<Product> Products { get; set; } = new();
+        public List<Category> Categories { get; set; } = new();
+        public List<Customer> Customers { get; set; } = new();
+        public List<Sale> Sales { get; set; } = new();
+        public List<SaleItem> SaleItems { get; set; } = new();
+        public List<PromoCode> PromoCodes { get; set; } = new();
+
+        private readonly string path;
+
+        public FileStorage(string Filepath)
+        {
+            path = Filepath;
+        }
+
+        public FileStorage Load()
+        {
+            if (!File.Exists(path))
+                return this;
+
+            var json = File.ReadAllText(path);
+
+            var storage = JsonSerializer.Deserialize<FileStorage>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            return storage ?? this;
+        }
+
+        public void Save()
+        {
+            var json = JsonSerializer.Serialize(this, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            File.WriteAllText(path, json);
+
+        }
+    }
+}
