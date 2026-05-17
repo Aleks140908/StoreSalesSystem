221d40aafb61f36523d5f02b78fa38dd0df87aae
Ailqq <aylasali243@gmail.com>
2026-05-17 10:20:41 +0300

    Add methods(AddCategory and EditCategory) in CategoryService class

--- Application/Services/CategoryService.cs
+++ Application/Services/CategoryService.cs
@@ -1,5 +1,4 @@
-﻿using StoreSalesSystem.Application.Interfaces;
-using StoreSalesSystem.Domain.Entities;
+﻿using StoreSalesSystem.Domain.Entities;
 using StoreSalesSystem.Infrastructure;
 using System;
 using System.Collections.Generic;
@@ -11,28 +10,13 @@ namespace StoreSalesSystem.Application.Services
 {
     public class CategoryService
     {
-        private readonly ICategoryRepository categoryRepo;
-        private readonly IProductRepository productRepo;
+        private readonly FileCategoryRepository categoryRepo;
+        private readonly FileProductRepository productRepo;
 
-        public CategoryService(ICategoryRepository categoryRepo, IProductRepository productRepo)
+        public CategoryService(FileCategoryRepository categoryRepo, FileProductRepository productRepo)
         {
             this.categoryRepo = categoryRepo;
             this.productRepo = productRepo;
         }
-<<<<<<< HEAD
-=======
-        public Category AddCategory(string name)
-        {
-            if (string.IsNullOrWhiteSpace(name))
-                throw new Exception("Задължително е да въведете име на категорията");
-
-            if (categoryRepo.GetAll().Any(c => c.Name.ToLower() == name.ToLower()))
-                throw new Exception("Категорията вече съществува");
-
-            var category = new Category(name);
-            return categoryRepo.Add(category);
-        }
-        
->>>>>>> 0c81fa4c0f0a9560bc19ad9f679ebed064bc16e5
     }
 }
